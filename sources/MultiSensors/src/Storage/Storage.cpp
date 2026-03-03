23e654499400d31db416c43b61b1d10800883545
Sasha7b9Work <Phuntik-it@tut.by>
2026-03-03 12:00:26 +0300

    *

--- sources/MultiSensors/src/Storage/Storage.cpp
+++ sources/MultiSensors/src/Storage/Storage.cpp
@@ -50,49 +50,38 @@ namespace Storage
     // Возвращает номер записи по её сквозному индексу
     static int NumberRecordForIndexRecord(int num_sector, int num_record);
 
-    struct Memory
+    // Возвращает true, если хранилище полностью заполнено - ни одной записи больше не влезет
+    bool IsFull();
+
+    int index_oldest = INT_MAX;         // Сквозной индекс самой старой записи Record
+    uint number_oldest = UINT_MAX;      // И номер данной записи
+
+    int index_newest = INT_MIN;         // Сквозной индекс записи Record с наибольшим номером
+    uint number_newest = 0;             // И номер данной записи
+
+    // Возвращает номер сектора, в котором хранится запись с данным индексом
+    static int NumberSectorForIndexRecord(int);
+
+    // В этой структуре хранится последний считанный из памятии сектор
+    struct MemorySector
     {
-        void Init();
+        int number = -1;                            // Номер сектора
+        MemBuffer<W25Q80DV::SIZE_SECTOR> buffer;    // Данные сектора
 
-        // Возвращает true, если хранилище полностью заполнено - ни одной записи больше не влезет
-        bool IsFull();
+        // Считывает данные из микросхемы
+        void Prepare(int number);
 
-    private:
+        // Записывает данные в микросхему. start - от начала сектора, size - сколько байт записать
+        void WriteData(uint start, int size);
 
-    friend struct Sector;
+        // Возвращает true, если нет ни одной валидной записи
+        bool IsEmpty();
 
-        int index_oldest = INT_MAX;         // Сквозной индекс самой старой записи Record
-        uint number_oldest = UINT_MAX;      // И номер данной записи
-
-        int index_newest = INT_MIN;         // Сквозной индекс записи Record с наибольшим номером
-        uint number_newest = 0;             // И номер данной записи
-
-        // Возвращает номер сектора, в котором хранится запись с данным индексом
-        int NumberSectorForIndexRecord(int) const;
-
-        // В этой структуре хранится последний считанный из памятии сектор
-        struct MemorySector
-        {
-            int number = -1;                            // Номер сектора
-            MemBuffer<W25Q80DV::SIZE_SECTOR> buffer;    // Данные сектора
-
-            // Считывает данные из микросхемы
-            void Prepare(int number);
-
-            // Записывает данные в микросхему. start - от начала сектора, size - сколько байт записать
-            void WriteData(uint start, int size);
-
-            // Возвращает true, если нет ни одной валидной записи
-            bool IsEmpty();
-
-            // Читает запись Record. Нумерация с начала сектора
-            void ReadRecord(int number, Record *);
-        };
-
-        MemorySector data_sector;
+        // Читает запись Record. Нумерация с начала сектора
+        void ReadRecord(int number, Record *);
     };
 
-    static Memory memory;
+    MemorySector data_sector;
 }
 
 
@@ -102,7 +91,7 @@ uint Storage::Record::CalculateCRC() const
 }
 
 
-int Storage::Memory::NumberSectorForIndexRecord(int index_record) const
+int Storage::NumberSectorForIndexRecord(int index_record)
 {
     return index_record / NUM_RECORDS_IN_SECTOR;
 }
@@ -120,7 +109,92 @@ int Storage::Record::Size() const
 }
 
 
-void Storage::Memory::Init()
+int Storage::NumberRecordForIndexRecord(int num_sector, int num_record)
+{
+    return num_record % (num_sector * NUM_RECORDS_IN_SECTOR);
+}
+
+
+bool Storage::IsFull()
+{
+    if (index_oldest == INT_MAX)        // Значит, нет ни одной записи
+    {
+        return false;
+    }
+
+    int number_sector_oldest = NumberSectorForIndexRecord(index_oldest);        // В этом секторе находится самая старая запись
+
+    int number_record = NumberRecordForIndexRecord(number_sector_oldest, index_oldest);
+
+    if (number_record == NUM_RECORDS_IN_SECTOR - 1)       // Если данная запись последняя в секторе, нужно стереть
+    {
+
+    }
+
+    return false;
+}
+
+
+void Storage::MemorySector::Prepare(int _number)
+{
+    if (number != _number)
+    {
+        number = _number;
+
+        buffer.Read((uint)number * W25Q80DV::SIZE_SECTOR);
+    }
+}
+
+
+bool Storage::MemorySector::IsEmpty()
+{
+    {
+        // Сначала проверим значения. Если все байты равны 0xFF, то в сектор после стирания ничего не записывалось
+
+        int num_not_FF = 0;                 // Количество байт, не равных 0xFF
+
+        for (int i = 0; i < buffer.Size(); i++)
+        {
+            if (data_sector.buffer.Data()[i] != 0xFF)
+            {
+                num_not_FF++;
+                break;
+            }
+        }
+
+        if (num_not_FF == 0)
+        {
+            return true;
+        }
+    }
+
+    {
+        // Теперь будем считывать записи и смотреть, есть ли хоть одна валидная
+
+        for (int num_record = 0; num_record < NUM_RECORDS_IN_SECTOR; num_record++)
+        {
+            Record record;
+
+            ReadRecord(num_record, &record);
+
+            if (record.IsValid())
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+void Storage::MemorySector::ReadRecord(int number_record, Record *record)
+{
+    std::memcpy(record, buffer.Data() + number_record * record->Size(), (uint)record->Size());
+}
+
+
+void Storage::Init()
 {
     number_oldest = UINT_MAX;
     index_oldest = INT_MAX;
@@ -164,97 +238,6 @@ void Storage::Memory::Init()
 }
 
 
-int Storage::NumberRecordForIndexRecord(int num_sector, int num_record)
-{
-    return num_record % (num_sector * NUM_RECORDS_IN_SECTOR);
-}
-
-
-bool Storage::Memory::IsFull()
-{
-    if (index_oldest == INT_MAX)        // Значит, нет ни одной записи
-    {
-        return false;
-    }
-
-    int number_sector_oldest = NumberSectorForIndexRecord(index_oldest);        // В этом секторе находится самая старая запись
-
-    int number_record = NumberRecordForIndexRecord(number_sector_oldest, index_oldest);
-
-    if (number_record == NUM_RECORDS_IN_SECTOR - 1)       // Если данная запись последняя в секторе, нужно стереть
-    {
-
-    }
-
-    return false;
-}
-
-
-void Storage::Memory::MemorySector::Prepare(int _number)
-{
-    if (number != _number)
-    {
-        number = _number;
-
-        buffer.Read((uint)number * W25Q80DV::SIZE_SECTOR);
-    }
-}
-
-
-bool Storage::Memory::MemorySector::IsEmpty()
-{
-    {
-        // Сначала проверим значения. Если все байты равны 0xFF, то в сектор после стирания ничего не записывалось
-
-        int num_not_FF = 0;                 // Количество байт, не равных 0xFF
-
-        for (int i = 0; i < buffer.Size(); i++)
-        {
-            if (memory.data_sector.buffer.Data()[i] != 0xFF)
-            {
-                num_not_FF++;
-                break;
-            }
-        }
-
-        if (num_not_FF == 0)
-        {
-            return true;
-        }
-    }
-
-    {
-        // Теперь будем считывать записи и смотреть, есть ли хоть одна валидная
-
-        for (int num_record = 0; num_record < NUM_RECORDS_IN_SECTOR; num_record++)
-        {
-            Record record;
-
-            ReadRecord(num_record, &record);
-
-            if (record.IsValid())
-            {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-
-void Storage::Memory::MemorySector::ReadRecord(int number_record, Record *record)
-{
-    std::memcpy(record, buffer.Data() + number_record * record->Size(), (uint)record->Size());
-}
-
-
-void Storage::Init()
-{
-    memory.Init();
-}
-
-
 void Storage::Update()
 {
 
@@ -263,7 +246,7 @@ void Storage::Update()
 
 void Storage::AppendMeasure(const Measure &measure)
 {
-    if (memory.IsFull())
+    if (IsFull())
     {
         EraseOldestSector();
     }
--- sources/MultiSensors/src/defines.cpp
+++ sources/MultiSensors/src/defines.cpp
@@ -2,8 +2,8 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 33
+#define VERSION_BUILD 34
 
-#define DATE_BUILD "2026-03-03 11:58:13"
+#define DATE_BUILD "2026-03-03 12:00:26"
 
 #endif
